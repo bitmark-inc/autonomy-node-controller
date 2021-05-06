2e9314b4652fd72a34055cdafb0a6d35bf78e2e3
Jim Yeh <jim@bitmark.com>
2021-05-06 18:08:04 +0800

    Improve the connection stability and the auth token expiry

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"flag"
 	"net/http"
+	"os"
+	"os/signal"
 	"time"
 
+	jwt "github.com/dgrijalva/jwt-go"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 
@@ -18,6 +22,9 @@ type RequestCommand struct {
 	Command string          `json:"command"`
 	Args    json.RawMessage `json:"args"`
 }
+
+// renewBefore: 10 minutes in seconds
+var renewBefore = 10 * time.Minute / time.Second
 
 func main() {
 	var configFile string
@@ -39,35 +46,92 @@ func main() {
 		WithField("created", created).
 		Info("controller initialized")
 
-	jwt, err := i.Auth()
-	if err != nil {
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	if err := i.Auth(); err != nil {
 		log.WithError(err).Panic("pod authentication fail")
 	}
 
-	messagingClient := messaging.New(
-		&http.Client{Timeout: 10 * time.Second},
-		viper.GetString("messaging.endpoint"),
-		jwt,
-		config.AbsoluteApplicationFilePath(viper.GetString("messaging.db_name")))
+	// The goroutine will continuously check auth_token and re-request a new one if
+	// a token is going to be expired.
+	go func(checkInterval time.Duration) {
+		for {
+			token := i.AuthToken()
+			if token == "" {
+				// wait for the first token to be acquired from the first authentication
+				time.Sleep(time.Second)
+				continue
+			}
 
-	if err := messagingClient.RegisterAccount(); err != nil {
-		log.Fatalf("registering account, error: %s", err)
-	}
+			// parse the jwt claim without verified the token signature
+			t, _, err := new(jwt.Parser).ParseUnverified(token, &jwt.StandardClaims{})
+			if err != nil {
+				log.WithError(err).Panic("fail to refresh token")
+			}
 
-	if err := messagingClient.RegisterKeys(); err != nil {
-		log.WithError(err).Fatalf("failed to register keys")
-	}
+			if claims, ok := t.Claims.(*jwt.StandardClaims); ok {
+				if time.Now().Unix() > (claims.ExpiresAt - int64(renewBefore)) {
+					if err := i.Auth(); err != nil {
+						log.WithError(err).Panic("fail to refresh token")
+					}
+					log.Info("successfully refresh a new token")
+				} else {
+					log.WithField("expires_at", claims.ExpiresAt).Debug("token not expired")
+				}
+				time.Sleep(checkInterval)
+				continue
+			} else {
+				log.WithError(errors.New("error casting token claims")).Panic("fail to refresh token")
+			}
+		}
+	}(time.Minute)
 
-	ws, err := messagingClient.NewWSClient()
-	if err != nil {
-		log.WithError(err).Panic("fail to establish websocket connection")
-	}
+CONNECTION_LOOP:
+	// This is the main loop for maintaining a persistant websocket connection to API server
+	for {
+		authToken := i.AuthToken()
 
-	for m := range ws.WhisperMessages() {
-		log.WithField("message", m).Debug("receive message")
-		responseMessage := controller.Process(m)
-		if responseMessage != nil {
-			ws.SendWhisperMessages(m.Source, m.SourceDevice, responseMessage)
+		messagingClient := messaging.New(
+			&http.Client{Timeout: 10 * time.Second},
+			viper.GetString("messaging.endpoint"),
+			authToken,
+			config.AbsoluteApplicationFilePath(viper.GetString("messaging.db_name")))
+
+		if err := messagingClient.RegisterAccount(); err != nil {
+			log.Fatalf("registering account, error: %s", err)
+		}
+
+		if err := messagingClient.RegisterKeys(); err != nil {
+			log.WithError(err).Fatalf("failed to register pre-keys on startup")
+		}
+
+		ws, err := messagingClient.NewWSClient()
+		if err != nil {
+			log.WithError(err).Panic("fail to establish websocket connection")
+		}
+
+		// This is a loop to watch and process new messages
+		for {
+			select {
+			case <-interrupt:
+				log.Info("service interrupted")
+				ws.Close()
+				break CONNECTION_LOOP
+			case m := <-ws.WhisperMessages():
+				// there will be a very last nil message if a connection is closed from the server
+				if m == nil {
+					log.Info("connection closed by server")
+					ws.Close()
+					break CONNECTION_LOOP
+				}
+
+				log.WithField("message", m).Debug("receive message")
+				responseMessage := controller.Process(m)
+				if responseMessage != nil {
+					ws.SendWhisperMessages(m.Source, m.SourceDevice, responseMessage)
+				}
+			}
 		}
 	}
 }
